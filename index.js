f8aaa503b44bf27f594030813e53a608aadf7fbc
ido-lempert <il.mrbit@gmail.com>
2025-03-19 16:07:55 +0200

    add lead

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ app.listen(app.get('port'));
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
-var received_updates = [];
+const token = process.env.TOKEN || 'token';
+const received_updates = [];
+const leads = [];
 
 app.get('/', function(req, res) {
     // console.log(req);
-    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre><hr><pr>' + JSON.stringify(leads, null, 2) + '</pr>');
 });
 
 app.get(['/facebook', '/instagram', '/threads'], function(req, res) {
@@ -28,6 +29,13 @@ app.get(['/facebook', '/instagram', '/threads'], function(req, res) {
     }
 });
 
+function getLead(data){
+    const accessToken = process.env.ACCESS_TOKEN;
+    fetch(`https://graph.facebook.com/v22.0/${data.entry[0].changes[0].value.leadgen_id}?fields=id,ad_id,form_id,created_time,field_data&access_token=${accessToken}`).then(async res => {
+        leads.unshift(await res.json());
+    }, err => console.error(err));
+}
+
 app.post('/facebook', function(req, res) {
     console.log('Facebook request body:', req.body);
 
@@ -40,6 +48,7 @@ app.post('/facebook', function(req, res) {
     console.log('request header X-Hub-Signature validated');
     // Process the Facebook updates here
     received_updates.unshift(req.body);
+    getLead(res.body);
     res.sendStatus(200);
 });
 
